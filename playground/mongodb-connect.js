553f1fc8604757a1246ea015c0af09b16bceaa96
Sergey V Osokin SVO <sergey8osokin@gmail.com>
2018-03-02 00:13:26 +0300

    added block concerning _id

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,12 +1,34 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
 
-MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, client) => {
     if (err) {
         return console.log("Unable to connect to MongoDB server.");
     }
     console.log("Connected to MongoDB server.");
 
-    db.collection("Todos").insertOne();
+    const db = client.db("TodoApp");    // get database reference
 
-    db.close();
-});
+    // db.collection("Todos").insertOne({
+    //     text: "Something to do",
+    //     completed: false
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log("Unable to insert todo: ", err);
+    //     }
+    //     console.log("success result: ", JSON.stringify(result.ops, undefined, 2));
+    // });
+    
+    // db.collection("Users").insertOne({
+    //     name: "Mike",
+    //     age: 32,
+    //     location: "Los-Angeles"
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log("Unable to insert user: ", err);
+    //     }
+    //     console.log("success result: ", result.ops[0]._id.getTimestamp());
+    // });
+  
+    client.close();
+}); 
